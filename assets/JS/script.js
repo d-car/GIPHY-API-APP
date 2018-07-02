a6c17512cae351dd64932f813ce2b6046bd0e5d7
unknown <danie@Dan-Laptop>
2018-07-02 15:43:57 -0500

    started js

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+var teamsArray = [];
+
+function displayGifs() {
+    var team = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl";
+}
+
+
