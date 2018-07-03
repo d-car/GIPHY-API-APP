cbfcc865ae8c58abbfdd28cf5df0813bead181e6
unknown <danie@Dan-Laptop>
2018-07-03 13:00:48 -0500

    buttons rendered

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,24 @@
-var teamsArray = [];
+var teams = ["Broncos", "Chiefs", "Chargers", "Raiders", "Texans", "Colts", "Jaguars", "Titans", "Ravens", "Bengals", "Browns", "Steelers", "Bills", "Dolphins", "Patriots", "Jets", "Cowboys", "Giants", "Eagles", "Redskins", "Bears", "Lions", "Packers", "Vikings", "Falcons", "Panthers", "Saints", "Buccaneers",];
 
-function displayGifs() {
-    var team = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl";
+
+
+
+
+
+//=================================================================================================================================
+
+function renderButtons() {
+    $("button-area").empty();
+
+    for (var i = 0; i < teams.length; i++) {
+        var button = $("<button>");
+            button.addClass("btn btn-primary");
+            button.attr("data-name", teams[i]);
+            button.text(teams[i]);
+        $("#button-area").append(button);
+    }
 }
+
+renderButtons();
 
 
