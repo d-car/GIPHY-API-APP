45cad4f064bc2b20564378974d9b57b12aafc136
unknown <danie@Dan-Laptop>
2018-07-03 14:38:40 -0500

    button clicks and ajax request working, fix undefined request

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,31 @@
 var teamArray = ["Broncos", "Chiefs", "Chargers", "Raiders", "Texans", "Colts", "Jaguars", "Titans", "Ravens", "Bengals", "Browns", "Steelers", "Bills", "Dolphins", "Patriots", "Jets", "Cowboys", "Giants", "Eagles", "Redskins", "Bears", "Lions", "Packers", "Vikings", "Falcons", "Panthers", "Saints", "Buccaneers",];
 
-$("button").on("click", function() {
-    var team = $(this).attr("data-person");
+//=================================================================================================================================
+
+function renderButtons() {
+    $("button-area").empty();
+
+    for (var i = 0; i < teamArray.length; i++) {
+        var button = $("<button>");
+            button.addClass("button");
+            button.attr("data-name", teamArray[i]);
+            button.text(teamArray[i]);
+        $("#button-area").append(button);
+    }
+}
+
+renderButtons();
+
+$(".button").on("click", function() {
+    var team = $(this).attr("data-team");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
 
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
+})
       .then(function(response) {
-        var results = response.data.url;
+        var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div class='item'>");
@@ -21,30 +37,7 @@ $("button").on("click", function() {
           $("#gif-area").prepend(gifDiv);
         }
       });
-  });
-
-    
-
-
-
-
-
-//=================================================================================================================================
-
-function renderButtons() {
-    $("button-area").empty();
-
-    for (var i = 0; i < teamArray.length; i++) {
-        var button = $("<button>");
-            button.addClass("button");
-            button.attr("id", "teamButtons")
-            button.attr("data-name", teamArray[i]);
-            button.text(teamArray[i]);
-        $("#button-area").append(button);
-    }
-}
-
-renderButtons();
+})
 
 
 
