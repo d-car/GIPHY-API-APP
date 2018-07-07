83212a6444ee2135ddfd95b1b3d39053144548e5
d-car <danielscarstensen@gmail.com>
2018-07-07 00:41:37 -0500

    added lots of comments in js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,9 +4,10 @@ var teamArray = ["Denver Broncos", "Kansas City Chiefs", "Los Angeles Chargers",
 
 //=================================================================================================================================
 
+//function empties the button div
 function renderButtons() {
     $("button-area").empty();
-
+//then loops through teamArray and creates a button for each index, adds class/attributes
     for (var i = 0; i < teamArray.length; i++) {
         var button = $("<button>");
             button.addClass("btn btn-primary");
@@ -14,100 +15,68 @@ function renderButtons() {
             button.text(teamArray[i]);
         $("#button-area").append(button);
     }
-
+//click event that empties the gif div and assigns queryURL for ajax call
     $(".btn").on("click", function() {
       $("#gif-area").empty();
         var team = $(this).attr("data-name");
         console.log(team);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
-  
+//ajax call to giphy API  
         $.ajax({
           url: queryURL,
           method: "GET"
     })
+//promise that runs the response function and grabs the data from the API    
           .then(function(response) {
             var results = response.data;
-  
+//looping through the results (response.data array, 10 indeces as specified in queryURL) and creating a div for each gif  
             for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div class='nflGif'>");
-  
-              var teamImage = $("<img>");
-              teamImage.attr("src", results[i].images.fixed_height_still.url);
-              teamImage.attr("data-state", "still");
-              gifDiv.html(teamImage);
-  
+              var gifDIV = $("<div class='unit' data-state='still'>");
+              var teamImg = $("<img id= 'teamImg'>");
+//assigning attributes to both the still and animated versions of the gif for click event later              
+              teamImg.attr("src", results[i].images.fixed_height.url);
+              teamImg.attr({'data-animate' : results[i].images.fixed_height.url});
+              teamImg.attr({"data-state" : "still"});
+              teamImg.attr({'data-still' : results[i].images.fixed_height_still.url});
+              gifDiv.html(teamImg);
+//prepends to gif div  
               $("#gif-area").prepend(gifDiv);
             }
           });
-    })
-
-    
+    })    
 }
 
-// function newTeamButton (); {
-//   var newTeam = $(this).attr("data-name");
-//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + newTeam + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
+// Adds new button from whatever is entered in search field
+$("#add-gif").on("click", function(event) {
+  event.preventDefault();
+  
+  var newTeam = $("#search-input").val().trim();
+  teamArray.push(newTeam);
+  console.log("Searched: " + newTeam);
+  $("#button-area").empty();
 
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//       $("#search-input").text(JSON.stringify(response));
-//       renderButtons();
-//     });
-// }
+  renderButtons();
+});
 
-  // $(".btn").on("click", function() {
-  //   $("#gif-area").empty();
-  //     var team = $(this).attr("data-name");
-  //     console.log(team);
-  //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
+//trying to pause/play gifs on click
+$(".unit").on("click", function() {
 
-  //     $.ajax({
-  //       url: queryURL,
-  //       method: "GET"
-  // })
-  //       .then(function(response) {
-  //         var results = response.data;
-
-  //         for (var i = 0; i < results.length; i++) {
-  //           var gifDiv = $("<div class='item'>");
-
-  //           var teamImage = $("<img>");
-  //           teamImage.attr("src", results[i].images.fixed_height.url);
-  //           gifDiv.html(teamImage);
-
-  //           $("#gif-area").prepend(gifDiv);
-  //         }
-  //       });
-  // })
-
-  $("#add-gif").on("click", function(event) {
-    event.preventDefault();
+  var state = $(this).attr('data-state');
     
-    var newTeam = $("#search-input").val().trim();
-    teamArray.push(newTeam);
-    console.log("Searched: " + newTeam);
-    $("#button-area").empty();
-  
-    renderButtons();
+    if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+    } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+    }
   });
+
   
+//render buttons on page load  
 renderButtons();
 
-$(".nflGif").on("click", function() {
 
-  var state = $(this).attr("data-state");
-  if (state === "still") {
-    $(this).attr("data-animate");
-    $(this).attr("data-state", "animate");
-  } else {
-    $(this).attr("data-still");
-    $(this).attr("data-state", "still");
-
-  }
-
-})
 
 
 
