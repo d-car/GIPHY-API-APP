09da30fc6092f8fd4d4a281d0b1b507831296db5
d-car <danielscarstensen@gmail.com>
2018-07-06 22:41:14 -0500

    search and new buttons functional

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,34 +14,87 @@ function renderButtons() {
             button.text(teamArray[i]);
         $("#button-area").append(button);
     }
+
+    $(".btn").on("click", function() {
+      $("#gif-area").empty();
+        var team = $(this).attr("data-name");
+        console.log(team);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
+  
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+    })
+          .then(function(response) {
+            var results = response.data;
+  
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div class='item'>");
+  
+              var teamImage = $("<img>");
+              teamImage.attr("src", results[i].images.fixed_height.url);
+              gifDiv.html(teamImage);
+  
+              $("#gif-area").prepend(gifDiv);
+            }
+          });
+    })
 }
 
+// function newTeamButton (); {
+//   var newTeam = $(this).attr("data-name");
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + newTeam + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response) {
+//       $("#search-input").text(JSON.stringify(response));
+//       renderButtons();
+//     });
+// }
+
+  // $(".btn").on("click", function() {
+  //   $("#gif-area").empty();
+  //     var team = $(this).attr("data-name");
+  //     console.log(team);
+  //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
+
+  //     $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  // })
+  //       .then(function(response) {
+  //         var results = response.data;
+
+  //         for (var i = 0; i < results.length; i++) {
+  //           var gifDiv = $("<div class='item'>");
+
+  //           var teamImage = $("<img>");
+  //           teamImage.attr("src", results[i].images.fixed_height.url);
+  //           gifDiv.html(teamImage);
+
+  //           $("#gif-area").prepend(gifDiv);
+  //         }
+  //       });
+  // })
+
+  $("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    
+    var newTeam = $("#search-input").val().trim();
+    teamArray.push(newTeam);
+    console.log("Searched: " + newTeam);
+    $("#button-area").empty();
+  
+    renderButtons();
+  });
+  
 renderButtons();
 
-$(".btn").on("click", function() {
-  $("#gif-area").empty();
-    var team = $(this).attr("data-name");
-    console.log(team);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=nfl+" + team + "&api_key=YtWIFNouKBztP0svyc6zF5A0mJFrGUSl&limit=10";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-})
-      .then(function(response) {
-        var results = response.data;
 
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div class='item'>");
 
-          var teamImage = $("<img>");
-          teamImage.attr("src", results[i].images.fixed_height.url);
-          gifDiv.html(teamImage);
-
-          $("#gif-area").prepend(gifDiv);
-        }
-      });
-})
 
 
 
